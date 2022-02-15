0606780da0b54bb4b284230c24c402f8c057fbcb
Eric Moran <emoran0403@gmail.com>
2022-02-15 16:09:24 -0500

    lab complete

--- app.js
+++ app.js
@@ -18,53 +18,97 @@
  * .then((result) = > { //? gets the result and passes it along
  * console.log(result) //? logs the result
  * return slowmath.method(result, c) //? returns the next function call.  doing this allows us to avoid the pyramid of doom
- * }).catch((error) => { //? catches any error from the previous step
- *  return console.log(error); //? logs that error
  * })
  * !-------------------
+ *
+ * repeating units are inside a try-catch block to account for any errors that may be thrown
  */
 
-try {
-  slowMath
-    .add(1, 1) // Add together 6 and 2
-    .then((result) => {
-      console.log(`log the result of adding 6 and 2 (8) = ${result}`); //log the result of adding 6 and 2 - gives 8
-      return slowMath.multiply(result, 2); // and then multiply the result by 2.
-    })
-    .then((result) => {
-      console.log(`log the result of multiplying by 2 (16) = ${result}`); // log the result of multiplying by 2 - gives 16
-      return slowMath.divide(result, 4); // and then divide the result by 4
-    })
-    .then((result) => {
-      console.log(`log the result of dividing by 4 (4) = ${result}`); // log the result of dividing by 4 - gives 4
-      return slowMath.subtract(result, 3); // and then subtract 3 from the result
-    })
-    .then((result) => {
-      console.log(`log the result of subtracting 3 (1) = ${result}`); // log the result of subtracting 3 - gives 1
-      return slowMath.add(result, 98); // and then add 98 to the result
-    })
-    .then((result) => {
-      console.log(`log the result of adding 98 (99) = ${result}`); // log the result of adding 98 - gives 1
-      return slowMath.remainder(result, 2); // and then find the remainder from dividing the result by 2
-    })
-    .then((result) => {
-      console.log(`log the result of finding the remainder from dividing by 2 (1) = ${result}`); // log the result of remainder from dividing the result by 2 - gives 1
-      return slowMath.multiply(result, 50); // and then multiply the result by 50.
-    })
-    .then((result) => {
-      console.log(`log the result of multiplying by 50 (50) = ${result}`); // log the result of multiplying by 50 - gives 50
-      return slowMath.remainder(result, 40); // and then find the remainder after dividing by 40.
-    })
-    .then((result) => {
-      console.log(`log the result of finding the remainder from dividing by 40 (10) = ${result}`); // log the result of finding the remainder from dividing by 40 - gives 10
-      return slowMath.add(result, 32); // and then add 32 to the result.
-    })
-    .then((result) => {
-      console.log(`The final result is: ${result}!!`); // final result - gives 42
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   slowMath
+//     .add(1, 1) // Add together 6 and 2
+//     .then((result) => {
+//       console.log(`log the result of adding 6 and 2 (8) = ${result}`); //log the result of adding 6 and 2 - gives 8
+//       return slowMath.multiply(result, 2); // and then multiply the result by 2.
+//     })
+//     .then((result) => {
+//       console.log(`log the result of multiplying by 2 (16) = ${result}`); // log the result of multiplying by 2 - gives 16
+//       return slowMath.divide(result, 4); // and then divide the result by 4
+//     })
+//     .then((result) => {
+//       console.log(`log the result of dividing by 4 (4) = ${result}`); // log the result of dividing by 4 - gives 4
+//       return slowMath.subtract(result, 3); // and then subtract 3 from the result
+//     })
+//     .then((result) => {
+//       console.log(`log the result of subtracting 3 (1) = ${result}`); // log the result of subtracting 3 - gives 1
+//       return slowMath.add(result, 98); // and then add 98 to the result
+//     })
+//     .then((result) => {
+//       console.log(`log the result of adding 98 (99) = ${result}`); // log the result of adding 98 - gives 1
+//       return slowMath.remainder(result, 2); // and then find the remainder from dividing the result by 2
+//     })
+//     .then((result) => {
+//       console.log(`log the result of finding the remainder from dividing by 2 (1) = ${result}`); // log the result of remainder from dividing the result by 2 - gives 1
+//       return slowMath.multiply(result, 50); // and then multiply the result by 50.
+//     })
+//     .then((result) => {
+//       console.log(`log the result of multiplying by 50 (50) = ${result}`); // log the result of multiplying by 50 - gives 50
+//       return slowMath.remainder(result, 40); // and then find the remainder after dividing by 40.
+//     })
+//     .then((result) => {
+//       console.log(`log the result of finding the remainder from dividing by 40 (10) = ${result}`); // log the result of finding the remainder from dividing by 40 - gives 10
+//       return slowMath.add(result, 32); // and then add 32 to the result.
+//     })
+//     .then((result) => {
+//       console.log(`The final result is: ${result}!!`); // final result - gives 42
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// } catch (error) {
+//   console.log(error);
+// }
+
+// async function doMath() {
+//   try {
+//     let result1 = await slowMath.add(6, 2);
+//     console.log(result1);
+//     let result2 = await slowMath.multiply(result1, 2);
+//     console.log(result2);
+//     let result3 = await slowMath.divide(result2, 4);
+//     console.log(result3);
+//     let result4 = await slowMath.subtract(result3, 3);
+//     console.log(result4);
+//     let result5 = await slowMath.add(result4, 98);
+//     console.log(result5);
+//     let result6 = await slowMath.remainder(result5, 2);
+//     console.log(result6);
+//     let result7 = await slowMath.multiply(result6, 50);
+//     console.log(result7);
+//     let result8 = await slowMath.remainder(result7, 40);
+//     console.log(result8);
+//     let result9 = await slowMath.add(result8, 32);
+//     console.log(result9);
+//   } catch (error) {}
+// }
+
+// doMath();
+
+// async function doMath2() {
+//   try {
+//     let result1 = await slowMath.exponent(2, 3);
+//     console.log(result1);
+//     let result2 = await slowMath.exponent(result1, 2);
+//     console.log(result2);
+//     let result3 = await slowMath.increment(result2);
+//     console.log(result3);
+//     let result4 = await slowMath.increment(result3);
+//     console.log(result4);
+//     let result5 = await slowMath.decrement(result4);
+//     console.log(result5);
+//     let result6 = await slowMath.exponent(result5, 2);
+//     console.log(result6);
+//   } catch (error) {}
+// }
+
+// doMath2();
--- async-math.js
+++ async-math.js
@@ -2,87 +2,144 @@
  * SlowMath Library
  * Covalence, LLC
  */
-let slowMath = (function() {
-    const wait = (delay) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve();
-            }, delay);
-        });
-    };
+let slowMath = (function () {
+  const wait = (delay) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+  };
 
-    function add(a, b) {
-        return wait(1000)
-        .then(() => {
-            return negativeCheck(a, b);
-        }).then((shouldResolve) => {
-            if (shouldResolve) {
-                return Promise.resolve(+a + +b);
-            } else {
-                return Promise.reject(new Error(`Error adding values ${a} and ${b}.`));
-            }
-        });
-    }
+  function add(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (shouldResolve) {
+          return Promise.resolve(+a + +b);
+        } else {
+          return Promise.reject(new Error(`Error adding values ${a} and ${b}.`));
+        }
+      });
+  }
 
-    function subtract(a, b) {
-        return wait(1000)
-        .then(() => {
-            return negativeCheck(a, b);
-        }).then((shouldResolve) => {
-            if (shouldResolve) {
-                return Promise.resolve(a - b);
-            } else {
-                return Promise.reject(new Error(`Error subtracting values ${a} and ${b}.`));
-            }
-        });
-    }
+  function subtract(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (shouldResolve) {
+          return Promise.resolve(a - b);
+        } else {
+          return Promise.reject(new Error(`Error subtracting values ${a} and ${b}.`));
+        }
+      });
+  }
 
-    function multiply(a, b) {
-        return wait(1000)
-        .then(() => {
-            return negativeCheck(a, b);
-        }).then((shouldResolve) => {
-            if (shouldResolve) {
-                return Promise.resolve(a * b);
-            } else {
-                return Promise.reject(new Error(`Error multiplying values ${a} and ${b}.`));
-            }
-        });
-    }
+  function multiply(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (shouldResolve) {
+          return Promise.resolve(a * b);
+        } else {
+          return Promise.reject(new Error(`Error multiplying values ${a} and ${b}.`));
+        }
+      });
+  }
 
-    function divide(a, b) {
-        return wait(1000)
-        .then(() => {
-            return negativeCheck(a, b);
-        }).then((shouldResolve) => {
-            if (+b === 0) {
-                return Promise.reject(new Error('Cannot divide by zero.'));
-            } else if (shouldResolve) {
-                return Promise.resolve(a / b);
-            } else {
-                return Promise.reject(new Error(`Error dividing ${a} by ${b}.`));
-            }
-        });
-    }
+  function divide(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (+b === 0) {
+          return Promise.reject(new Error("Cannot divide by zero."));
+        } else if (shouldResolve) {
+          return Promise.resolve(a / b);
+        } else {
+          return Promise.reject(new Error(`Error dividing ${a} by ${b}.`));
+        }
+      });
+  }
 
-    function remainder(a, b) {
-        return wait(1000)
-        .then(() => {
-            return negativeCheck(a, b);
-        }).then((shouldResolve) => {
-            if (+b === 0) {
-                return Promise.reject(new Error('Cannot divide by zero for remainder.'));
-            } else if (shouldResolve) {
-                return Promise.resolve(a % b);
-            } else {
-                return Promise.reject(new Error(`Error dividing ${a} by ${b} for remainder.`));
-            }
-        });
-    }
+  function remainder(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (+b === 0) {
+          return Promise.reject(new Error("Cannot divide by zero for remainder."));
+        } else if (shouldResolve) {
+          return Promise.resolve(a % b);
+        } else {
+          return Promise.reject(new Error(`Error dividing ${a} by ${b} for remainder.`));
+        }
+      });
+  }
 
-    function negativeCheck(a, b) {
-        return a > -1 && b > -1;
-    }
+  //*  I copied the remainder function and changed the IF statement to check for 'a' being positive, since 0 to any power is silly
 
-    return { add, subtract, multiply, divide, remainder };
+  function exponent(a, b) {
+    return wait(1000)
+      .then(() => {
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (+a === 0) {
+          return Promise.reject(new Error("Cannot raise 0 to a power."));
+        } else if (shouldResolve) {
+          return Promise.resolve(a ** b);
+        } else {
+          return Promise.reject(new Error(`Error raising ${a} to the power of ${b}.`));
+        }
+      });
+  }
+
+  //* Increment and Decrement were giving me unexpected errors.
+  //* First, i tried just 'a++' in the resolve, but that did not give me a+1
+  //* My next error occured when i tried to remove the first '.then' since the negative check is unnecessary, but it would not console log anything
+
+  function increment(a) {
+    return wait(1000)
+      .then(() => {
+        let b = 10;
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (shouldResolve) {
+          return Promise.resolve(a + 1);
+        } else {
+          return Promise.reject(new Error(`Error incrementing ${a}.`));
+        }
+      });
+  }
+
+  function decrement(a) {
+    return wait(1000)
+      .then(() => {
+        let b = 10;
+        return negativeCheck(a, b);
+      })
+      .then((shouldResolve) => {
+        if (shouldResolve) {
+          return Promise.resolve(a - 1);
+        } else {
+          return Promise.reject(new Error(`Error decrementing ${a}.`));
+        }
+      });
+  }
+
+  function negativeCheck(a, b) {
+    return a > -1 && b > -1;
+  }
+
+  return { add, subtract, multiply, divide, remainder, exponent, increment, decrement };
 })();
